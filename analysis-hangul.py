10db28d7ccd0629af3c75843921c8433aaae4a46
Youngho Cho <dominoyh5@gmail.com>
2020-04-25 18:32:53 +0900

    Update analysis-hangul.py

--- analysis-hangul.py
+++ analysis-hangul.py
@@ -1,3 +1,17 @@
-ONSET = []
-NUCLEUS = []
-CODA = []
+ONSET = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+NUCLEUS = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅠ', 'ㅘ', 'ㅛ', 'ㅙ', 'ㅚ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅡ', 'ㅢ', 'ㅣ']
+CODA = ['채움', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+def hangul_to_code(hangul):
+	result = []
+	semi_code = ord(hangul) - ord('가')
+
+	result.append(semi_code // 588)
+	result.append((semi_code % 588) // 28)
+	result.append(semi_code % 28)
+
+	return result
+
+if __name__ == '__main__':
+	print(hangul_to_code('한'))
+	print(hangul_to_code('글'))
